03cdc94523d69f8fe585d7ae4bd1acd50e723651
Rockott <777888000d@gmail.com>
2019-10-27 17:11:21 +0200

    Binding to custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   serverElements = [{ type: 'server', name: 'Test server', content: 'Just a test!' }];
 
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBluePrintAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'bluePrint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
 
 }
